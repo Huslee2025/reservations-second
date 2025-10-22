e79e626fcc3dfe242a04c042dd0bb9e6c689fd68
Khussein <iccpid2025@gmail.com>
2025-10-22 11:20:48 +0200

    Added notification system

--- src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
@@ -17,12 +17,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import be.iccbxl.pid.reservationsspringboot.model.Artist;
 import be.iccbxl.pid.reservationsspringboot.service.ArtistService;
 
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 import be.iccbxl.pid.reservationsspringboot.model.Artist;
 import be.iccbxl.pid.reservationsspringboot.service.ArtistService;
+
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class ArtistController {
@@ -33,89 +34,102 @@ public class ArtistController {
 
 	@GetMapping("/artists")
 	public String index(Model model) {
-	    List<Artist> artists = service.getAllArtists();
-	    model.addAttribute("artists", artists);
-	    return "artist/index";
+		List<Artist> artists = service.getAllArtists();
+		model.addAttribute("artists", artists);
+		return "artist/index";
 	}
-	
-	@GetMapping("/artists/{id}")
-    public String show(Model model, @PathVariable("id") long id) {
-	Artist artist = service.getArtist(id);
 
-	model.addAttribute("artist", artist);
-	model.addAttribute("title", "Fiche d'un artiste");
-		
-        return "artist/show";
-    }
-	
+	@GetMapping("/artists/{id}")
+	public String show(Model model, @PathVariable("id") long id) {
+		Artist artist = service.getArtist(id);
+
+		model.addAttribute("artist", artist);
+		model.addAttribute("title", "Fiche d'un artiste");
+
+		return "artist/show";
+	}
+
 	@GetMapping("/artists/{id}/edit")
 	public String edit(Model model, @PathVariable long id, HttpServletRequest request) {
 		Artist artist = service.getArtist(id);
 
 		model.addAttribute("artist", artist);
 
-
-		//Générer le lien retour pour l'annulation
+		// Générer le lien retour pour l'annulation
 		String referrer = request.getHeader("Referer");
 
-		if(referrer!=null && !referrer.equals("")) {
+		if (referrer != null && !referrer.equals("")) {
 			model.addAttribute("back", referrer);
 		} else {
-			model.addAttribute("back", "/artists/"+artist.getId());
+			model.addAttribute("back", "/artists/" + artist.getId());
 		}
-		
+
 		return "artist/edit";
+	}
+
+
+	@PutMapping("/artists/{id}/edit")
+	public String update(@Valid @ModelAttribute Artist artist, BindingResult bindingResult,
+		@PathVariable long id, Model model, RedirectAttributes redirAttrs) {
+	    if (bindingResult.hasErrors()) {
+		model.addAttribute("errorMessage", "Échec de la modification de l'artiste !");
+			
+		return "artist/edit";
+	    }
+		
+	    Artist existing = service.getArtist(id);
+		
+	    if(existing==null) {
+		    return "artist/index";
+	    }		
+	
+	    service.updateArtist(id, artist);
+	    redirAttrs.addFlashAttribute("successMessage", "Artiste modifié avec succès.");
+    
+	    return "redirect:/artists/"+artist.getId();
 	}
 	
 	@GetMapping("/artists/create")
 	public String create(Model model) {
-		Artist artist = new Artist();
-
-		model.addAttribute("artist", artist);
-
-		return "artist/create";
-	}
-
-	@PutMapping("/artists/{id}/edit")
-	public String update(@Valid @ModelAttribute Artist artist, BindingResult bindingResult, @PathVariable long id, Model model) {
-	    
-		if (bindingResult.hasErrors()) {
-			return "artist/edit";
-		}
-		
-		Artist existing = service.getArtist(id);
-		
-		if(existing==null) {
-			return "artist/index";
-		}		
-	
-	    	service.updateArtist(id, artist);	    
-    
-		return "redirect:/artists/"+artist.getId();
+	    if (!model.containsAttribute("artist")) {
+	        model.addAttribute("artist", new Artist());
+    }
+	    		
+	    return "artist/create";
 	}
 
 
 	@PostMapping("/artists/create")
-	public String store(@Valid @ModelAttribute Artist artist, BindingResult bindingResult, Model model) {
+	public String store(@Valid @ModelAttribute Artist artist, BindingResult bindingResult, 
+		Model model, RedirectAttributes redirAttrs) {
+	    
+	    if (bindingResult.hasErrors()) {
+	    	model.addAttribute("errorMessage", "Échec de la création de l'artiste !");
+	    	
+	    	return "artist/create";
+	    }
+		    
+	    service.addArtist(artist);
+	    redirAttrs.addFlashAttribute("successMessage", "Artiste créé avec succès.");
 
-		if (bindingResult.hasErrors()) {
-			return "artist/create";
-		}
-
-		service.addArtist(artist);
-
-		return "redirect:/artists/" + artist.getId();
+	    return "redirect:/artists/"+artist.getId();
 	}
+
 
 	@DeleteMapping("/artists/{id}")
-	public String delete(@PathVariable long id, Model model) {
-		Artist existing = service.getArtist(id);
-
-		if (existing != null) {
-			service.deleteArtist(id);
-		}
-
-		return "redirect:/artists";
+	public String delete(@PathVariable long id, Model model, RedirectAttributes redirAttrs) {
+	    Artist existing = service.getArtist(id);
+		
+	    if(existing!=null) {		
+	    	service.deleteArtist(id);
+	    	
+	    	redirAttrs.addFlashAttribute("successMessage", "Artiste supprimé avec succès.");
+	    } else {
+	    	redirAttrs.addFlashAttribute("errorMessage", "Échec de la suppression de l'artiste !");
+	    }
+	    	    
+	    return "redirect:/artists";
 	}
+
 
 }
