386100294c0733a57f7046345370db974ddf1984
Khussein <iccpid2025@gmail.com>
2025-11-05 13:34:59 +0100

    Spring Security advanced configuration

--- src/main/java/be/iccbxl/pid/reservationsspringboot/config/SpringSecurityConfig.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/config/SpringSecurityConfig.java
@@ -1,53 +1,61 @@
 package be.iccbxl.pid.reservationsspringboot.config;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 import be.iccbxl.pid.reservationsspringboot.service.CustomUserDetailsService;
 
 @Configuration
-@EnableMethodSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig {
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
 
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+		return new BCryptPasswordEncoder(BCryptVersion.$2Y, 12);
 	}
 
 	@Bean
-	public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder)
-			throws Exception {
-		AuthenticationManagerBuilder authMngrBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-		authMngrBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    public AuthenticationManager authenticationManager(
+            final AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
-		return authMngrBuilder.build();
-	}
 
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http.authorizeHttpRequests(auth -> {
-			auth.requestMatchers("/", "/login", "/logout", "/css/**", "/js/**").permitAll();
-			auth.requestMatchers("/admin").hasRole("ADMIN");
-			auth.requestMatchers("/user").hasRole("USER");
-			auth.anyRequest().authenticated();
-		})
+    public SecurityFilterChain configure(final HttpSecurity http) throws Exception {
+        return http.cors(Customizer.withDefaults())
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> {
+                	auth.requestMatchers("/admin").hasRole("ADMIN");
+                	auth.requestMatchers("/user").hasRole("MEMBER");
+                	auth.anyRequest().permitAll();
+                })
+                .formLogin(form -> form
+                    .loginPage("/login")
+                    .usernameParameter("login")
+                    .failureUrl("/login?loginError=true"))
+                .logout(logout -> logout
+                    .logoutSuccessUrl("/login?logoutSuccess=true")
+                    .deleteCookies("JSESSIONID"))
+                .exceptionHandling(exception -> exception
+                    .authenticationEntryPoint(
+			new LoginUrlAuthenticationEntryPoint("/login?loginRequired=true")))
+                .build();
+    }
 
-				.formLogin(form -> form.loginPage("/login").usernameParameter("login")
-						.failureUrl("/login?loginError=true"))
-				.logout(logout -> logout.logoutSuccessUrl("/login?logoutSuccess=true").deleteCookies("JSESSIONID"))
-				.exceptionHandling(exception -> exception
-						.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login?loginRequired=true")))
-				.build();
-	}
 
 }
