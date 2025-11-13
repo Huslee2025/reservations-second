cf924b530c5a014f186c1f06bec81b66577988e8
Khussein <iccpid2025@gmail.com>
2025-11-13 20:20:31 +0100

    Profile details

--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/UserRole.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/UserRole.java
@@ -1,19 +1,16 @@
 package be.iccbxl.pid.reservationsspringboot.model;
 
-public enum UserRole {	
-	  ADMIN("admin"),
-	  MEMBER("member"),
-	  AFFILIATE("affiliate"),
-	  PRESS("press"),
-	  PRODUCER("producer");
+public enum UserRole {
+	ADMIN("Administrateur"), MEMBER("Membre"), AFFILIATE("Affilié"), PRESS("Critique de presse"),
+	PRODUCER("Producteur");
 
-	  private String role;
+	private String role;
 
-	  UserRole(String role) {
-	    this.role = role;
-	  }
+	UserRole(String role) {
+		this.role = role;
+	}
 
-	  public String getValue() {
-	    return role;
-	  }
+	public String getValue() {
+		return role;
+	}
 }
