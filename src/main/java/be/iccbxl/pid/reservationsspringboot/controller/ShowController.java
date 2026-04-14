af119439c41ac5a33567f5ff5c9daf3c60b08463
Khussein <iccpid2025@gmail.com>
2026-04-14 20:11:04 +0200

    Update templates Show (added artist's list)

--- src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ShowController.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ShowController.java
@@ -1,12 +1,17 @@
 package be.iccbxl.pid.reservationsspringboot.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import be.iccbxl.pid.reservationsspringboot.model.Artist;
+import be.iccbxl.pid.reservationsspringboot.model.ArtistType;
 import be.iccbxl.pid.reservationsspringboot.model.Show;
 import be.iccbxl.pid.reservationsspringboot.service.ShowService;
 
@@ -38,6 +43,20 @@ public class ShowController {
 	    return "redirect:/shows";
 	}
 
+	// Récupérer les artistes du spectacle et les grouper par type
+	Map<String, ArrayList<Artist>> collaborateurs = new TreeMap<>();
+
+	for (ArtistType at : show.getArtistTypes()) {
+	    String type = at.getType().getType();
+
+	    if (collaborateurs.get(type) == null) {
+		collaborateurs.put(type, new ArrayList<>());
+	    }
+
+	    collaborateurs.get(type).add(at.getArtist());
+	}
+
+	model.addAttribute("collaborateurs", collaborateurs);
 	model.addAttribute("show", show);
 	model.addAttribute("title", "Fiche d'un spectacle");
 	model.addAttribute("module", "shows");
