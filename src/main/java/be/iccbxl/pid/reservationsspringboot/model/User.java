43594ec8ce55f0bfcacb49482afe7a034d1ff52c
Khussein <iccpid2025@gmail.com>
2026-04-22 17:19:09 +0200

    Relation ManyToMany between User & Representation

--- src/main/java/be/iccbxl/pid/reservationsspringboot/model/User.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/model/User.java
@@ -11,16 +11,25 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
-@NoArgsConstructor
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(exclude = { "roles", "representations" })
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(lombok.AccessLevel.NONE)
+    @EqualsAndHashCode.Include
     private Long id;
 
     @Column(unique = true, nullable = false)
@@ -29,24 +38,38 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String firstname;
+
+    @Column(nullable = false)
     private String lastname;
 
     @Column(unique = true, nullable = false)
     private String email;
 
+    @Column(nullable = false, length = 2)
     private String langue;
 
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime created_at = LocalDateTime.now();
+
+    @Column(name = "updated_at")
     private LocalDateTime updated_at;
+
+    @Column(name = "email_verified_at")
     private LocalDateTime email_verified_at;
+
+    @Column(name = "remember_token")
     private String remember_token;
 
     @ManyToMany(mappedBy = "users")
     private List<Role> roles = new ArrayList<>();
 
+    @ManyToMany(mappedBy = "users")
+    private List<Representation> representations = new ArrayList<>();
+
     public User addRole(Role role) {
-	if (!this.roles.contains(role)) {
+	if (role != null && !this.roles.contains(role)) {
 	    this.roles.add(role);
 	    role.addUser(this);
 	}
@@ -54,9 +77,25 @@ public class User {
     }
 
     public User removeRole(Role role) {
-	if (this.roles.contains(role)) {
+	if (role != null && this.roles.contains(role)) {
 	    this.roles.remove(role);
 	    role.getUsers().remove(this);
+	}
+	return this;
+    }
+
+    public User addRepresentation(Representation representation) {
+	if (representation != null && !this.representations.contains(representation)) {
+	    this.representations.add(representation);
+	    representation.addUser(this);
+	}
+	return this;
+    }
+
+    public User removeRepresentation(Representation representation) {
+	if (representation != null && this.representations.contains(representation)) {
+	    this.representations.remove(representation);
+	    representation.getUsers().remove(this);
 	}
 	return this;
     }
